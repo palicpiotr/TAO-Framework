dce7f932dc459b10031f01f92a0f0f0d5a95da3c
Piotr Palic <skipper_tm@outlook.com>
2019-01-07 17:09:12 +0300

    perspective, raw

---
+++ GL.PalicPG.Prim_117/GL.PalicPG.Prim_117.LW7/TexturesForObject.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Tao.DevIl;
+using Tao.OpenGl;
+
+namespace GL.PalicPG.Prim_117.LW7
+{
+    public class TexturesForObject
+    {
+        public TexturesForObject()
+        { }
+
+        private string texture_name = "";
+        private int imageId = 0;
+        private uint mGlTextureObject = 0;
+        public uint GetTextureObj() => mGlTextureObject;
+
+        public void LoadTextureForModel(string FileName)
+        {
+            texture_name = FileName;
+            Il.ilGenImages(1, out imageId);
+            Il.ilBindImage(imageId);
+            string url = "";
+            url = AppDomain.CurrentDomain.BaseDirectory;
+            url += "\\";
+            url += texture_name;
+            if (Il.ilLoadImage(url))
+            {
+                int width = Il.ilGetInteger(Il.IL_IMAGE_WIDTH);
+                int height = Il.ilGetInteger(Il.IL_IMAGE_HEIGHT);
+                int bitspp = Il.ilGetInteger(Il.IL_IMAGE_BITS_PER_PIXEL);
+                switch (bitspp)
+                {
+                    case 24:
+                        mGlTextureObject = MakeGlTexture(Gl.GL_RGB, Il.ilGetData(), width, height);
+                        break;
+                    case 32:
+                        mGlTextureObject = MakeGlTexture(Gl.GL_RGBA, Il.ilGetData(), width, height);
+                        break;
+                }
+                Il.ilDeleteImages(1, ref imageId);
+            }
+        }
+
+        private static uint MakeGlTexture(int Format, IntPtr pixels, int w, int h)
+        {
+            Gl.glGenTextures(1, out uint texObject);
+            Gl.glPixelStorei(Gl.GL_UNPACK_ALIGNMENT, 1);
+            Gl.glBindTexture(Gl.GL_TEXTURE_2D, texObject);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_S, Gl.GL_REPEAT);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_T, Gl.GL_REPEAT);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_LINEAR);
+            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_LINEAR);
+            Gl.glTexEnvf(Gl.GL_TEXTURE_ENV, Gl.GL_TEXTURE_ENV_MODE, Gl.GL_REPLACE);
+            switch (Format)
+            {
+                case Gl.GL_RGB:
+                    Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGB, w, h, 0, Gl.GL_RGB, Gl.GL_UNSIGNED_BYTE, pixels);
+                    break;
+
+                case Gl.GL_RGBA:
+                    Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGBA, w, h, 0, Gl.GL_RGBA, Gl.GL_UNSIGNED_BYTE, pixels);
+                    break;
+            }
+            return texObject;
+        }
+
+    }
+}
---
+++ GL.PalicPG.Prim_117/GL.PalicPG.Prim_117.LW8/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GL.PalicPG.Prim_117.LW8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
